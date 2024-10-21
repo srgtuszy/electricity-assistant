839df5636bc8f775ca7de581a7dd8e6560ff5b4f
Michał Tuszyński <srgtuszy@gmail.com>
2024-10-21 19:05:18 +0200

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:electricity_assistant/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ElectricityAssistantApp());
 }
 
